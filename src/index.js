a8d0291d25927e28d209a07ac8164396c9bbfa83
mahdi <mahdi.313.ab@gmail.com>
2023-12-07 14:00:24 +0330

    Add html-loader, file-loader, and clean-webpack-plugin - Added html-loader to automatically require the files we reference in img tags - Added file-loader to handle the svg,png,jpg,gif files from our images - Configured file-loader to add our images to an imgs directory in dist - Also configured it to add a hash to their filenames - Lastly, added clean-webpack-plugin to our production config to clean out the dist directory each time we build - Warning!: file-loader is deprecated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { AlertService } from "./app/alert.service";
 import { run } from "./app/app";
 import { ComponentService } from "./app/component.service";
 import "./main.scss";
-
+alert("mahdi");
 const alertService = new AlertService();
 const componentService = new ComponentService();
 run(alertService, componentService);
