c345e3bd002fba729722a6e01470ee818f0da44f
mahdi <mahdi.313.ab@gmail.com>
2023-12-07 14:12:13 +0330

    Add entrypoint for vendor js file, add bootstrap js - Now we have 2 entry files and 2 bundles - The vendor file houses code that is less likely to change, 3rd party libraries - The main file has all of our app code

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  entry: "./src/index.js",
+  entry: { main: "./src/index.js", vendor: "./src/vendor.js" },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
